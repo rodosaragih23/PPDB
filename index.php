b8a8cbe4e5d5f9f0485f65d0045da3bfe3a598b8
rodosaragih23 <48644454+rodosaragih23@users.noreply.github.com>
2022-02-01 22:42:40 +0700

    PPDB SMP

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<!-- Favicons -->
+<link href="images/logo.png" rel="icon" />
+<html lang="en">
+  <!--<![endif]-->
+  <head>
+    <meta charset="utf-8" />
+    <title>SMP Negeri 5 Kota Tasikmalaya</title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+    />
+    <link rel="stylesheet" media="all" href="css/style.css" />
+    <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header id="header">
+      <div class="container">
+        <a href="index.php" id="logo" title="HarrisonHighSchool"
+          >HarrisonHighSchool</a
+        >
+        <div class="menu-trigger"></div>
+        <nav id="menu">
+          <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="#visi_misi">Visi Misi</a></li>
+            <li><a href="#sambutan">Sambutan</a></li>
+          </ul>
+          <ul>
+            <li><a href="register.php"> Daftar Penerimaan Siswa Baru</a></li>
+            <li><a href="login.php">Login</a></li>
+          </ul>
+        </nav>
+        <!-- / navigation -->
+      </div>
+      <!-- / container -->
+    </header>
+    <!-- / header -->
+
+    <div class="slider">
+      <ul class="bxslider">
+        <li>
+          <div class="container">
+            <div class="info">
+              <h2>
+                Selamat Datang di
+                <br /><span>SMP Negeri 5 Tasikmalaya</span>
+              </h2>
+              <a href="#pic"></a>
+            </div>
+          </div>
+          <!-- / content -->
+        </li>
+      </ul>
+      <div class="bg-bottom"></div>
+    </div>
+
+    <section class="posts">
+      <div class="container">
+        <h2 id="visi_misi">Visi dan Misi</h2>
+        <article>
+          <div class="pic">
+            <img width="121" src="images/2.png" alt="" />
+          </div>
+          <div class="info">
+            <h3>VISI</h3>
+            <p>
+              Visi SMP Negeri 5 Tasikmalaya “Membentuk insan Religius, Unggul,
+              Berkarakter serta Berbudaya Lingkungan”
+            </p>
+          </div>
+        </article>
+        <article>
+          <div class="pic"><img width="121" src="images/3.png" alt="" /></div>
+          <div class="info">
+            <h3>Misi</h3>
+            <p>- Membangun kultur daya sekolah religius.</p>
+            <p>
+              - Mendesain pengelolaan sekolah yang unggul di tataran nasional.
+            </p>
+            <p>
+              - Meningkatkan layanan progres pembelajaran abad 21 berbasis HOTS
+            </p>
+            <p>
+              - Menyelenggarakan pendidikan layanan khusus CI dan kelang
+              unggulan.
+            </p>
+            <p>
+              - Memiliki peserta didik yang kreatif, kompetitif, dan unggul baik
+              akademik maupun non akademik serta mampu bersaing dalam memilih
+              SMA/SMK unggulan.
+            </p>
+            <p>
+              - Membentuk siswa yang berkarakter yang mengacu pada lima pilar
+              utama Religius, Nasionalis, Mandiri, Gotong Royong dan Integritas.
+            </p>
+            <p>- Membentuk sekolah adiwiyata yang sehat, sejuk, dan indah.</p>
+          </div>
+        </article>
+      </div>
+      <!-- / container -->
+    </section>
+
+    <section class="news" id="sambutan">
+      <div class="container">
+        <h2>Sambutan Kepala Sekolah</h2>
+        <article>
+          <div class="sambutan">
+            <img width="200" src="images/kepsek smp5.png" alt="" />
+          </div>
+        </article>
+        <article>
+          <div class="info">
+            <h4>Hj. YUYUN SITI NOOERHAESIH, S.Pd., M.Pd</h4>
+            <p>
+              &nbsp; Puji dan syukur kita panjatkan ke hadirat Allah SWT.
+              Alhamdulillah berkat dukungan dari seluruh stake holder, sekolah
+              kita sudah memiliki website. Website ini merupakan media informasi
+              dan komunikasi edukasi SMP Negeri 5 Tasikmalaya terhadap publik.
+              Web ini mengandung konten informasi akademik, kegiatan ekstra
+              kurikuler, prestasi siswa dan guru baik akademik maupun non
+              akademik, event-event terbaru yang terjadi di SMP Negeri 5
+              Tasikmalaya, PPDB, link edukasi, dll. Semoga dengan adanya web
+              site ini, sekolah kita terus eksis, meningkat prestasi dan
+              kulaitas pendidikannya.
+            </p>
+          </div>
+        </article>
+      </div>
+      <!-- / container -->
+    </section>
+
+    <footer id="footer">
+      <div class="container">
+        <section>
+          <article class="col-1">
+            <h3>Contact</h3>
+            <ul>
+              <li class="address">
+                <a href="https://goo.gl/maps/uNbnpUF5fYshHM2HA"
+                  >Jalan RE Martadinata No. 85 Kota Tasikmalaya Jawa Barat<br />Detroit,
+                  MI 48226</a
+                >
+              </li>
+              <li class="mail">
+                <a href="#">info@smpn5tasikmalaya.sch.id</a>
+              </li>
+              <li class="phone last"><a>(0265) 330277</a></li>
+            </ul>
+          </article>
+          <article class="col-2">
+            <h3>Social media</h3>
+            <p>
+              Temporibus autem quibusdam et aut debitis aut rerum necessitatibus
+              saepe.
+            </p>
+            <ul>
+              <li class="facebook"><a href="#">Facebook</a></li>
+              <li class="google-plus"><a href="#">Google+</a></li>
+              <li class="twitter"><a href="#">Twitter</a></li>
+              <li class="pinterest"><a href="#">Pinterest</a></li>
+            </ul>
+          </article>
+        </section>
+      </div>
+      <div class="copyright">
+        <p class="copy">Copyright 2021 SMP Negeri 5 Tasikmalaya.</p>
+      </div>
+      <!-- / container -->
+    </footer>
+    <!-- / footer -->
+
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script>
+      window.jQuery ||
+        document.write("<script src='js/jquery-1.11.1.min.js'>\x3C/script>");
+    </script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,120 @@
+<?php
+session_start();
+include 'dbconnect.php';
+$alert = '';
+
+if(isset($_SESSION['role'])){
+	header("location:index.php");
+}
+
+if(isset($_POST['btn-daftar']))
+{
+ $email = mysqli_real_escape_string($conn,$_POST['email']);
+ $password = mysqli_real_escape_string($conn,md5($_POST['password']));
+
+ //cek apakah email sudah pernah digunakan
+$lihat1 = mysqli_query($conn,"select * from user where useremail='$email'");
+$lihat2 = mysqli_num_rows($lihat1);
+ 
+if($lihat2 < 1){
+    //email belum pernah digunakan
+    $insert = mysqli_query($conn,"insert into user (useremail,userpassword) values ('$email','$password')");
+        
+        //eksekusi query
+        if($insert){
+            echo "<div class='alert alert-success'>Berhasil mendaftar, silakan login.</div>
+            <meta http-equiv='refresh' content='2; url= login.php'/>  ";
+        } else {
+            //daftar gagal
+            echo "<div class='alert alert-warning'>Gagal mendaftar, silakan coba lagi.</div>
+            <meta http-equiv='refresh' content='2'>";
+        }
+
+    }
+ else
+    {
+    //jika email sudah pernah digunakan
+    $alert = '<strong><font color="red">Email sudah pernah digunakan</font></strong>';
+    echo '<meta http-equiv="refresh" content="2">';
+    }
+ 
+};
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pendaftaran PSB</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144808195-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-144808195-1');
+	</script>
+    <script src="jquery.min.js"></script>
+	<style>body{background-color:#2c3e50;}
+	@media screen and (max-width: 600px) {
+h4{font-size:85%;}
+}
+.container{
+	background-color:#008bc4;
+	width:50%;
+	border: 3px white;
+	border-style:solid;
+	border-radius:30px;
+	padding-top:3%;
+	padding-bottom:2%;
+}
+.btn{
+	width:40%;
+}
+	</style>
+	<link rel="icon" 
+      type="image/png" 
+      href="favicon.png">
+  </head>
+  <body>
+  
+  <div align="center">
+  
+  
+  
+
+    <a href="index.php"><img src="logo.png" width="10%" style="margin-top:2%" \></a>
+
+	<br \><br \>
+			<div class="container">
+					<div style="color:white">
+					<h2>Pendaftaran PSB Online</h2><br \>
+                    <label><?php echo $alert ?></label>
+					</div>
+                <form method="post">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email" name="email" autofocus required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password" name="password" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="btn-daftar">Daftar</button>
+                    <a class="btn btn-info text-light" href="login.php">Login</a>
+                </form>
+			
+			<br \>
+        </div></div>
+  </body>
+</html>
